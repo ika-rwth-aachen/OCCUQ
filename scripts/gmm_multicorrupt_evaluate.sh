ba746f4a8a2eef6369d99ff0dcaeb293efad9ce8
Till Beemelmanns <till.beemelmanns@gmail.com>
2025-03-12 09:54:10 +0000

    add ood detection evalutation script and add results

--- scripts/gmm_multicorrupt_evaluate.sh
+++ scripts/gmm_multicorrupt_evaluate.sh
@@ -19,7 +19,7 @@ $weight \
 
 # MultiCorrupt Evaluation
 corruptions=("snow" "fog" "motionblur" "brightness" "missingcamera")
-levels=("3" "2" "1")
+levels=("1" "2" "3")
 
 for corruption in "${corruptions[@]}"; do
     for level in "${levels[@]}"; do
@@ -30,3 +30,89 @@ for corruption in "${corruptions[@]}"; do
         --overwrite_nuscenes_root=/workspace/multicorrupt/$corruption/$level
     done
 done
+
+python scripts/ood_detection_evaluation.py \
+--work_dirs /workspace/work_dirs/occuq_mlpv5_sn
+
+
+# Mean Evaluation
+#         Measure  mAUROC  mAUPR  mFPR95
+#             GMM   80.15  79.43   56.18
+# Softmax Entropy   54.63  56.21   94.47
+#     Max Softmax   56.16  57.52   93.17
+
+#         Measure  snow_1_AUROC  snow_1_AUPR  snow_1_FPR95
+#             GMM         62.84        62.28         85.58
+# Softmax Entropy         50.87        52.00         95.40
+#     Max Softmax         51.20        52.89         95.68
+
+#         Measure  snow_2_AUROC  snow_2_AUPR  snow_2_FPR95
+#             GMM         91.30        91.53         39.41
+# Softmax Entropy         51.44        56.50         99.12
+#     Max Softmax         53.70        58.56         98.60
+
+#         Measure  snow_3_AUROC  snow_3_AUPR  snow_3_FPR95
+#             GMM         99.85        99.86          0.30
+# Softmax Entropy         82.81        85.88         79.10
+#     Max Softmax         85.98        88.17         68.72
+
+#         Measure  fog_1_AUROC  fog_1_AUPR  fog_1_FPR95
+#             GMM        61.81       60.44        86.01
+# Softmax Entropy        52.61       54.28        93.97
+#     Max Softmax        53.03       54.77        93.89
+
+#         Measure  fog_2_AUROC  fog_2_AUPR  fog_2_FPR95
+#             GMM        72.39       71.91        77.62
+# Softmax Entropy        56.62       59.39        94.22
+#     Max Softmax        57.61       60.13        93.79
+
+#         Measure  fog_3_AUROC  fog_3_AUPR  fog_3_FPR95
+#             GMM        91.47       91.92        41.09
+# Softmax Entropy        69.27       73.36        90.78
+#     Max Softmax        71.15       74.82        88.97
+
+#         Measure  motionblur_1_AUROC  motionblur_1_AUPR  motionblur_1_FPR95
+#             GMM               69.25              68.81               79.61
+# Softmax Entropy               46.75              47.61               96.25
+#     Max Softmax               47.70              48.45               96.06
+
+#         Measure  motionblur_2_AUROC  motionblur_2_AUPR  motionblur_2_FPR95
+#             GMM               82.76              82.26               59.35
+# Softmax Entropy               45.16              46.46               98.50
+#     Max Softmax               46.94              47.77               97.99
+
+#         Measure  motionblur_3_AUROC  motionblur_3_AUPR  motionblur_3_FPR95
+#             GMM               90.92              90.88               40.21
+# Softmax Entropy               49.38              50.71               96.84
+#     Max Softmax               51.74              52.58               95.50
+
+#         Measure  brightness_1_AUROC  brightness_1_AUPR  brightness_1_FPR95
+#             GMM               60.96              60.10               87.49
+# Softmax Entropy               49.80              49.23               95.30
+#     Max Softmax               50.06              49.35               95.40
+
+#         Measure  brightness_2_AUROC  brightness_2_AUPR  brightness_2_FPR95
+#             GMM               72.35              72.57               78.73
+# Softmax Entropy               50.10              49.99               97.47
+#     Max Softmax               50.78              50.41               97.39
+
+#         Measure  brightness_3_AUROC  brightness_3_AUPR  brightness_3_FPR95
+#             GMM               91.79              92.29               37.98
+# Softmax Entropy               54.95              55.75               99.67
+#     Max Softmax               56.67              56.79               99.37
+
+#         Measure  missingcamera_1_AUROC  missingcamera_1_AUPR  missingcamera_1_FPR95
+#             GMM                  71.80                 67.67                  70.51
+# Softmax Entropy                  53.94                 53.75                  93.11
+#     Max Softmax                  54.85                 54.40                  92.39
+
+#         Measure  missingcamera_2_AUROC  missingcamera_2_AUPR  missingcamera_2_FPR95
+#             GMM                  87.32                 84.34                  40.77
+# Softmax Entropy                  54.98                 55.71                  92.57
+#     Max Softmax                  56.81                 57.33                  90.93
+
+#         Measure  missingcamera_3_AUROC  missingcamera_3_AUPR  missingcamera_3_FPR95
+#             GMM                  95.39                 94.54                  18.03
+# Softmax Entropy                  50.82                 52.49                  94.78
+#     Max Softmax                  54.22                 56.36                  92.91
+
---
+++ scripts/ood_detection_evaluation.py
@@ -0,0 +1,84 @@
+import numpy as np
+from sklearn.metrics import average_precision_score, roc_curve, auc
+import os
+import pandas as pd
+import argparse
+
+
+def calculate_auroc(predictions, targets):
+    fpr, tpr, thresholds = roc_curve(y_true=targets, y_score=predictions)
+    roc_auc = auc(fpr, tpr)
+    fpr_best = 0
+    threshold = 0
+    for i, j, threshold in zip(tpr, fpr, thresholds):
+        if i > 0.95:
+            fpr_best = j
+            break
+    return roc_auc, fpr_best, threshold
+
+
+def main(work_dirs):
+    corruption_types = ["snow", "fog", "motionblur", "brightness", "missingcamera"]
+    corruptions = [f"{corruption}_{i}" for corruption in corruption_types for i in range(1, 4)]
+    measures = ["gmm_uncertainty_per_sample", "softmax_entropy_per_sample", "max_softmax_per_sample"]
+    nice_measure_names = ["GMM", "Softmax Entropy", "Max Softmax"]
+    scale = "_scale3"
+    all_corruptions_available = True
+
+    results = []
+    for measure in measures:
+        measure_results = {"Measure": nice_measure_names[measures.index(measure)]}
+        
+        # id data
+        id_file = f'{work_dirs}/clean{scale}/{measure}.csv'
+        id_density = np.loadtxt(id_file)
+        id_labels = np.ones(id_density.shape[0])
+        
+        for corruption in corruptions:
+            ood_file = f'{work_dirs}/{corruption}{scale}/{measure}.csv'
+            
+            if not os.path.isfile(ood_file):
+                all_corruptions_available = False
+                continue
+            
+            ood_density = np.loadtxt(ood_file)
+            ood_labels = np.zeros(ood_density.shape[0])
+            
+            labels = np.concatenate((id_labels, ood_labels))
+            scores = np.concatenate((id_density, ood_density))
+            
+            if measure == "max_softmax_per_sample":
+                scores = 1 - scores
+            
+            auroc, fpr_best, _ = calculate_auroc(scores, labels)
+            aupr = average_precision_score(labels, scores)
+            
+            measure_results[f"{corruption}_AUROC"] = auroc * 100
+            measure_results[f"{corruption}_AUPR"] = aupr * 100
+            measure_results[f"{corruption}_FPR95"] = fpr_best * 100
+        
+        results.append(measure_results)
+
+    df = pd.DataFrame(results)
+    for corruption in corruptions:
+        print(df[["Measure", f"{corruption}_AUROC", f"{corruption}_AUPR", f"{corruption}_FPR95"]].round(2).to_string(index=False))
+        print()
+
+    if all_corruptions_available:
+        df["mAUROC"] = df[[f"{corruption}_AUROC" for corruption in corruptions]].mean(axis=1)
+        df["mAUPR"] = df[[f"{corruption}_AUPR" for corruption in corruptions]].mean(axis=1)
+        df["mFPR95"] = df[[f"{corruption}_FPR95" for corruption in corruptions]].mean(axis=1)
+
+        print(df[["Measure", "mAUROC", "mAUPR", "mFPR95"]].round(2).to_string(index=False))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='OOD Detection Evaluation')
+    parser.add_argument(
+        '--work_dirs',
+        type=str,
+        default='/workspace/work_dirs/occuq_mlpv5_sn',
+        help='Working directories'
+    )
+    args = parser.parse_args()
+    main(args.work_dirs)
